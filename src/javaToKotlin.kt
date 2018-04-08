fc2e05839c0f2664b71593d0f4a2ce6ec7d1c58d
sunil <plsr.sunil@gmail.com>
2018-04-08 12:37:58 -0500

    Initial commit

---
+++ src/javaToKotlin.kt
@@ -0,0 +1,39 @@
+class JavaCode {
+    fun toJSON(collection: Collection<Int>): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        val iterator = collection.iterator()
+        while (iterator.hasNext()) {
+            val element = iterator.next()
+            sb.append(element)
+            if (iterator.hasNext()) {
+                sb.append(", ")
+            }
+        }
+        sb.append("]")
+        return sb.toString()
+    }
+}
+
+/*
+This code is automatically converted to kotlin, by the IDE
+* public class JavaCode {
+    public String toJSON(Collection<Integer> collection) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Iterator<Integer> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            sb.append(element);
+            if (iterator.hasNext()) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}*/
+
+/*
+* Integer in java is converted in Int
+* */
